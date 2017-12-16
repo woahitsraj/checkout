74b6429b487bfdf87cc00f6660227a0762e37005
Rajan Singh <rajan1992@hotmail.com>
2017-12-16 12:30:05 -0500

    Setup Jest testing Added enzyme Modified eslint configuration for Jes

--- src/components/ProductsHeader.js
+++ src/components/ProductsHeader.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import './ProductsHeader.css'
+
 const ProductsHeader = () => {
   return (
     <header className="products-header">
