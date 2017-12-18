0f48aa874549d0fa6b737f1d20ce1d8f8c85f599
Rajan Singh <rajan1992@hotmail.com>
2017-12-17 21:41:06 -0500

    added some more modal animation

--- src/components/ModalFooter.js
+++ src/components/ModalFooter.js
@@ -4,7 +4,11 @@ import PropTypes from 'prop-types'
 import './ModalFooter.css'
 
 const ModalFooter = ({ children }) => {
-  return <div className="modal-footer">{children}</div>
+  return (
+    <div className="modal-footer">
+      {children}
+    </div>
+  )
 }
 
 ModalFooter.propTypes = {
